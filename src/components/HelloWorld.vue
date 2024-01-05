c56d192fc589adb2e6814833629762c7551cacbc
ihugang <ihugang@gmail.com>
2024-01-05 21:14:06 +0800

    inited

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1> Hi, I am Hu Gang.</h1>
+  <h2> Wechat/QQ: 390652</h2>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
